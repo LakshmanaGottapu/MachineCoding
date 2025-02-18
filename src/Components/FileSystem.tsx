98f4694abdfb98ba3d2c1b27be1c891eba3924f2
lakshman <lakshman.automation@gmail.com>
2025-02-18 21:13:47 +0530

    amateur filesystem is developed.

--- src/Components/FileSystem.tsx
+++ src/Components/FileSystem.tsx
@@ -1,20 +1,18 @@
-import {Dispatch, MouseEvent, SetStateAction, useState, MutableRefObject} from 'react'
+import {Dispatch, MouseEvent, SetStateAction, useState} from 'react'
 import { FileType } from "../Pages/FolderPage"
 
-function FileSystem({data, level=1, setFileContent, displayIdRef}:{data:FileType, level?:number, setFileContent:Dispatch<SetStateAction<string>>, displayIdRef:MutableRefObject<number>}) {
+function FileSystem({data, level=1, setActiveItems, setActiveItem}:{data:FileType, level?:number, setActiveItems:Dispatch<SetStateAction<FileType[]>>, setActiveItem:Dispatch<SetStateAction<FileType | undefined>>}) {
     const [folderOpen, setFolderOpen] = useState(false);
     function displayContent(e:MouseEvent){
         e.stopPropagation();
-        if(!data.isFolder && data.content!==undefined){
-            displayIdRef.current = data.id
-            setFileContent(data.content)
-        }
+        if(!data.isFolder && data.content!==undefined)
+            setActiveItem(data);
     }
     return (
         <div style={{marginLeft: '1rem', width:`${(20-level)}vw`}} onClick={displayContent}>
-            <header onClick={()=>setFolderOpen(prev => !prev)} style={{cursor:'pointer', border:'1px solid black', margin:'5px', padding:'5px', }}> {data.isFolder ? (folderOpen ? '📂' :'📁') : '🗄️'} <strong>{data.name}</strong>{ data.isFolder && ( folderOpen ?  <span style={{float:'right'}}>🔽</span> : <span style={{float:'right'}}>▶️</span>) }</header>
+            <header onClick={()=>setFolderOpen(prev => !prev)} style={{userSelect:'none', cursor:'pointer', border:'1px solid black', margin:'5px', padding:'5px', }}> {data.isFolder ? (folderOpen ? '📂' :'📁') : '🗄️'} <strong>{data.name}</strong>{ data.isFolder && ( folderOpen ?  <span style={{float:'right', userSelect:'none'}}>🔽</span> : <span style={{float:'right', userSelect:'none'}}>▶️</span>) }</header>
             
-            {data.isFolder && folderOpen && data.items!=undefined && data.items.map((item, index) => <FileSystem key={index} data={item} level={level+1} setFileContent={setFileContent} displayIdRef={displayIdRef}/>)}
+            {data.isFolder && folderOpen && data.items!=undefined && data.items.map((item, index) => <FileSystem key={index} data={item} level={level+1} setActiveItems={setActiveItems} setActiveItem={setActiveItem}/>)}
         </div>
     )
 }
--- src/Pages/FolderPage.tsx
+++ src/Pages/FolderPage.tsx
@@ -1,13 +1,15 @@
-import {useState, useRef} from 'react';
+import {useState, useEffect} from 'react';
 import FileSystem from '../Components/FileSystem';
+import './vscode.css';
 export type FileType = {
     id:number, name: string, items?: FileType[], isFolder: boolean, content?: string
 }
 
-
 function FolderPage() {
     const [fileContent, setFileContent] = useState('');
-    const displayIdRef = useRef(-1);
+    const [activeItem, setActiveItem] = useState<FileType>();
+    const [showDashboard, setShowDashboard] = useState(false);
+    const [activeItems, setActiveItems] = useState<FileType[]>([]);
     const [data, setData] = useState<FileType>({
         id:1,
         name: 'machinecoding',
@@ -27,7 +29,7 @@ function FolderPage() {
                                 id:3,
                                 name: 'names.txt',
                                 isFolder: false,
-                                content: 'lakshman, saikumar, naveen, prasanth, chaithanya, veena, jyothi'
+                                content: 'lakshman, chaithanya, premchand, saikumar'
                             }
                         ]
                     },
@@ -35,7 +37,7 @@ function FolderPage() {
                         id:4,
                         name: 'phonenumbers.txt',
                         isFolder: false,
-                        content: '8309951482, 8372836019, 8374042584, 7981812142'
+                        content: '8309951482, 8919940485, 8372836019, 8374042584, 7981812142'
                     },
                     {
                         id:5,
@@ -60,12 +62,9 @@ function FolderPage() {
         ]
     })
     function handleFileInput(){
-        if(displayIdRef.current > -1){
-            const node = findNode(data, displayIdRef.current);
-            if(node != undefined){
-               node.content = fileContent;
-               setData({...data})
-            }
+        if(activeItem !==undefined){
+            activeItem.content = fileContent;
+            setData({...data});
         }
     }
     function findNode(data:FileType, id:number):FileType | undefined{
@@ -78,15 +77,36 @@ function FolderPage() {
             }
         }
     }
-    return (<section style={{height: '90vh', display:"flex", border:'1px solid black', padding:'5px', margin:'5px'}}>
-        <section style={{width:'20vw', borderRight:'2px solid black', paddingRight:'1vw'}}>
-            <FileSystem data={data} setFileContent={setFileContent} displayIdRef={displayIdRef}/>
+    useEffect(()=>{
+        if(activeItem && activeItem.content)
+            setFileContent(activeItem.content)
+        setActiveItems(prev => {
+            if(activeItem == undefined)
+                return [];
+            else {
+                if(prev.indexOf(activeItem) < 0){
+                    prev.push(activeItem);
+                }
+                return [...prev];
+            }
+        })
+    },[activeItem])
+    return (
+        <section style={{height: '90vh', display:"flex", border:'1px solid black', padding:'5px', margin:'5px'}}>
+            <section style={{width:'3rem', border:'1px solid black', display:'flex', flexDirection:'column'}}>
+                <span style={{cursor:'pointer', padding:'0.4rem', userSelect:'none'}} onDoubleClick={()=>setShowDashboard(prev => !prev)}>🗃️</span>
+            </section>
+            {showDashboard && <aside style={{width:'20vw', borderRight:'2px solid black', paddingRight:'1vw'}}>
+                <FileSystem data={data} setActiveItems={setActiveItems} setActiveItem={setActiveItem}/>
+            </aside>}
+            <section style={{minWidth: '70vw', marginLeft:'1rem'}}>
+                <nav style={{width:'70vw', display:'flex'}}>
+                    { activeItems.map(item => <nav className="item" onClick={()=>setActiveItem(item)} style={{border:'1px solid black', backgroundColor:item==activeItem ? 'lightgray' : '' }} >{item.name}</nav>) }
+                </nav>
+                <textarea name="" id="" cols={157} rows={46} onChange={(e)=>setFileContent(e.target.value)} value={fileContent} style={{padding:'15px'}} onBlur={handleFileInput}>
+                </textarea>
+            </section>
         </section>
-        <section style={{ marginLeft:'1rem'}}>
-            <textarea name="" id="" cols={157} rows={46} onChange={(e)=>setFileContent(e.target.value)} value={fileContent} style={{padding:'15px'}} onBlur={handleFileInput}>
-            </textarea>
-        </section>
-    </section>
     )
 }
 
